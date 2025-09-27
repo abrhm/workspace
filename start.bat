f288e62291e23f57be3b1c43b7290d44a84fec0d
Gabor, Abraham <godgab@gmail.com>
2025-09-27 22:41:39 +0200

    Always mount devenv, accept multiple parameters

--- start.bat
+++ start.bat
@@ -1,12 +1,20 @@
 @echo off
-if "%1"=="" (
-    echo Error: Please provide a folder path to mount
-    echo Usage: start.bat ./path/to/folder
-    exit /b 1
-)
+setlocal EnableDelayedExpansion
 
 REM Build the Docker image
 docker build -t devenv .
 
-REM Run the Docker container with the mounted volume
-docker run --rm -it -v "%1:/home/user/work/%~n1" devenv
+REM Start building the docker run command with the current directory mount
+set "cmd=docker run --rm -it -v "%~dp0:/devenv""
+
+REM Process all parameters as additional mounts
+:param_loop
+if "%1" neq "" (
+    set "cmd=!cmd! -v "%1:/home/user/work/%~n1""
+    shift
+    goto param_loop
+)
+
+REM Add the image name and execute
+set "cmd=!cmd! devenv"
+%cmd%
