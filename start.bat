c099bad90d82c3bfec3b8fc7b83d455b28384a4b
Gabor, Abraham <godgab@gmail.com>
2025-09-27 22:45:58 +0200

    Fix work folder mounting

--- start.bat
+++ start.bat
@@ -10,7 +10,14 @@ set "cmd=docker run --rm -it -v "%~dp0:/devenv""
 REM Process all parameters as additional mounts
 :param_loop
 if "%1" neq "" (
-    set "cmd=!cmd! -v "%1:/home/user/work/%~n1""
+    REM Get the full path and remove trailing backslash if present
+    set "fullpath=%~f1"
+    if "!fullpath:~-1!"=="\" set "fullpath=!fullpath:~0,-1!"
+    
+    REM Get just the folder name without trailing backslash
+    for %%I in ("!fullpath!") do set "foldername=%%~nxI"
+    
+    set "cmd=!cmd! -v "!fullpath!:/home/user/work/!foldername!""
     shift
     goto param_loop
 )
